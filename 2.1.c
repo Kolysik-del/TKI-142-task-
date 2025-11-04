b19a4e167f1dbb3eb5087207ec13d699afd5eb00
Уваров Николай <auvarovi.nikola2007@gmail.com>
2025-11-04 17:18:17 +0300

    2.1.c

--- 2.1.c
+++ 2.1.c
@@ -4,24 +4,35 @@
  * @param a - первая переменная
  * @param b - вторая переменная
  * @param c - третья переменная
+ * @param choice - переменная для выбора варианта
  * @return Вернет 0, если программа выполнена корректно. Иначе - 1
  */
-int main()
-{
-    double a, b;
-    printf("Введите два числа a и b:");
-    scanf("%lf %lf", &a, &b);
-    printf("До обмена: a = %.2lf, b = %.2lf\n", a, b);
-    double c = a;
-    a = b;
-    b = c;
-    printf("После обмена с переменной c: a = %.2lf, b = %.2lf\n", a, b);
-    printf("Введите два числа a и b:");
-    scanf("%lf %lf", &a, &b);
-    printf("До обмена: a = %.2lf, b = %.2lf\n", a, b);
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    printf("После обмена без переменной c: a = %.2lf, b = %.2lf\n", a, b);
+int main() {
+    int a, b, c, choice;
+    printf("Введите значение a:");
+    scanf_s("%d", &a);
+    printf("Введите значение b:");
+    scanf_s("%d", &b);
+    printf("1. С использованием третьей переменной\n");
+    printf("2. Без использования третьей переменной\n");
+    printf("Введите номер варианта:");
+    scanf_s("%d", &choice);
+    if (choice == 1)
+    {
+        c = a;
+        a = b;
+        b = c;
+    }
+    else if (choice == 2)
+    {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+    else
+    {
+        return 0;
+    }
+    printf("После обмена: a = %d, b = %d\n", a, b);
     return 0;
 }
