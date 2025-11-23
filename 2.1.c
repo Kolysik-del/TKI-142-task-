e3440903ef91f1371702388f7d10526def6ba274
Уваров Николай <auvarovi.nikola2007@gmail.com>
2025-11-23 15:42:43 +0300

    2.1.c

--- 2.1.c
+++ 2.1.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
 /**
- * @brief меняет между собой значения переменных a и b с использованием 3 переменной c и без неё
+ * @brief Меняет местами значения переменных a и b с использованием 3 переменной c и без неё
  * @param a - первая переменная
  * @param b - вторая переменная
  * @param c - третья переменная
  * @param choice - переменная для выбора варианта
  * @return Вернет 0, если программа выполнена корректно. Иначе - 1
  */
+typedef enum {
+    with = 1,
+    without
+} swap;
+void swapWith(int* a, int* b, int* c) {
+    *c = *a;
+    *a = *b;
+    *b = *c;
+}
+void swapWithout(int* a, int* b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
 int main() {
     int a, b, c, choice;
-    printf("Введите значение a:");
+    printf("Введите значение a: ");
     scanf_s("%d", &a);
-    printf("Введите значение b:");
+    printf("Введите значение b: ");
     scanf_s("%d", &b);
     printf("1. С использованием третьей переменной\n");
     printf("2. Без использования третьей переменной\n");
-    printf("Введите номер варианта:");
+    printf("Введите номер варианта: ");
     scanf_s("%d", &choice);
-    if (choice == 1)
-    {
-        c = a;
-        a = b;
-        b = c;
-    }
-    else if (choice == 2)
-    {
-        a = a + b;
-        b = a - b;
-        a = a - b;
-    }
-    else
-    {
-        return 0;
+    switch (choice) {
+    case with:
+        swapWith(&a, &b, &c);
+        break;
+    case without:
+        swapWithout(&a, &b);
+        break;
+    default:
+        printf("Ошибка: неверный номер варианта.\n");
+        return 1;
     }
     printf("После обмена: a = %d, b = %d\n", a, b);
     return 0;
