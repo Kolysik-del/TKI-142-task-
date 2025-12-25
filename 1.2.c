a643a40f729ed921ad7cab8906a66092f8073205
Уваров Николай <auvarovi.nikola2007@gmail.com>
2025-12-25 17:09:20 +0300

    1.2.c

--- 1.2.c
+++ 1.2.c
@@ -8,17 +8,25 @@
  * @param y2 Координата y второй точки
  * @return Расстояние между точками
  */
-double distance(double x1, double y1, double x2, double y2) {
+double distance(const double x1, const double y1, const double x2, const double y2);
+/**
+ * @brief Точка входа в программу
+ * @return Вернет 0, если программа выполнена корректно. Иначе - 1
+ */
+int main(void) 
+{
+    system("chcp 1251");
+    double x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    printf("Введите координаты первой точки (x1, y1): ");
+    scanf_s("%lf %lf", &x1, &y1);
+    printf("Введите координаты второй точки (x2, y2): ");
+    scanf_s("%lf %lf", &x2, &y2);
+    printf("Расстояние равно %.2f\n", distance(x1, y1, x2, y2));
+    return 0;
+}
+double distance(const double x1, const double y1, const double x2, const double y2)
+{
     double dx = x2 - x1;
     double dy = y2 - y1;
     return sqrt(dx * dx + dy * dy);
-}
-int main(void) {
-    double x1, y1, x2, y2;
-    printf("Введите координаты первой точки (x1, y1): ");
-    scanf("%lf %lf", &x1, &y1);
-    printf("Введите координаты второй точки (x2, y2): ");
-    scanf("%lf %lf", &x2, &y2);
-    printf("Расстояние равно %.2f\n", distance(x1, y1, x2, y2));
-    return 0;
 }
