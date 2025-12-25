f38ad38c8e2e31deedda43b984178df99c6e2fd7
Уваров Николай <auvarovi.nikola2007@gmail.com>
2025-12-25 17:15:38 +0300

    1.3.c

--- 1.3.c
+++ 1.3.c
@@ -6,18 +6,24 @@
  * @param g ускорение свободного падения
  * @return возвращает рассчитанное давление, производимое бетонной плитой
  */
-double davka(const double weight, const double square, const double g)
-{
-    return (weight * g) / square;
-}
+double davka(const double weight, const double square, const double g);
+/**
+ * @brief Точка входа в программу
+ * @return Вернет 0, если программа выполнена корректно. Иначе - 1
+ */
 int main(void)
 {
+    system("chcp 1251");
     double weight, square;
     const double g = 9.8;
     printf("Введите значение массы тела (кг): ");
-    scanf("%lf", &weight);
+    scanf_s("%lf", &weight);
     printf("Введите значение площади опоры (м²): ");
-    scanf("%lf", &square);
+    scanf_s("%lf", &square);
     printf("Давление тела на поверхность равно: %.4lf Па\n", davka(weight, square, g));
     return 0;
+}
+double davka(const double weight, const double square, const double g)
+{
+    return (weight * g) / square;
 }
